4d0e2f5fe8ee8e5de320b1217c1c543817dc8297
sofiabukovskaya <sofi454545@gmail.com>
2020-12-21 14:11:23 +0200

    Update

--- app/src/main/java/com/example/navigationdrawerapp/Fragment_child.java
+++ app/src/main/java/com/example/navigationdrawerapp/Fragment_child.java
@@ -1,5 +1,4 @@
 package com.example.navigationdrawerapp;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +7,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
-public class Fragment_eight extends Fragment {
+
+public class Fragment_child extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_eigth, container,false);
+        return inflater.inflate(R.layout.fragment_child, container,false);
     }
 }
